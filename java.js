c055816415cd3cc32fe1f0d0b7a5c0406436defa
ExsuantermyJJV <106329150+ExsuantermyJJV@users.noreply.github.com>
2023-05-02 12:24:47 -0500

    Update java.js

--- java.js
+++ java.js
@@ -30,7 +30,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('may 1, 2023 00:00:00').getTime(),
+let countDown = new Date('may 3, 2023 00:00:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
